ae6a7a338fa0777d1810240807092ac7c8542aa0
Ivan Lu (Monash University) <ivan.lu1997@gmail.com>
2020-10-20 18:12:23 +1100

    Changed splits, refactored dataset, training and testing loop, and put metrics into a function

---
+++ shared/meta.py
@@ -0,0 +1,420 @@
+import torch
+from torch import nn
+from torch import optim
+from torch.nn import functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from torch import optim
+import numpy as np
+from copy import deepcopy
+
+
+# TODO: Need to Reference to https://github.com/dragen1860/MAML-Pytorch
+class Learner(nn.Module):
+    """
+
+    """
+
+    def __init__(self, config, imgc, imgsz):
+        """
+
+        :param config: network config file, type:list of (string, list)
+        :param imgc: 1 or 3
+        :param imgsz:  28 or 84
+        """
+        super(Learner, self).__init__()
+
+
+        self.config = config
+
+        # this dict contains all tensors needed to be optimized
+        self.vars = nn.ParameterList()
+        # running_mean and running_var
+        self.vars_bn = nn.ParameterList()
+
+        for i, (name, param) in enumerate(self.config):
+            if name is 'conv2d':
+                # [ch_out, ch_in, kernelsz, kernelsz]
+                w = nn.Parameter(torch.ones(*param[:4]))
+                # gain=1 according to cbfin's implementation
+                torch.nn.init.kaiming_normal_(w)
+                self.vars.append(w)
+                # [ch_out]
+                self.vars.append(nn.Parameter(torch.zeros(param[0])))
+
+            elif name is 'convt2d':
+                # [ch_in, ch_out, kernelsz, kernelsz, stride, padding]
+                w = nn.Parameter(torch.ones(*param[:4]))
+                # gain=1 according to cbfin's implementation
+                torch.nn.init.kaiming_normal_(w)
+                self.vars.append(w)
+                # [ch_in, ch_out]
+                self.vars.append(nn.Parameter(torch.zeros(param[1])))
+
+            elif name is 'linear':
+                # [ch_out, ch_in]
+                w = nn.Parameter(torch.ones(*param))
+                # gain=1 according to cbfinn's implementation
+                torch.nn.init.kaiming_normal_(w)
+                self.vars.append(w)
+                # [ch_out]
+                self.vars.append(nn.Parameter(torch.zeros(param[0])))
+
+            elif name is 'bn':
+                # [ch_out]
+                w = nn.Parameter(torch.ones(param[0]))
+                self.vars.append(w)
+                # [ch_out]
+                self.vars.append(nn.Parameter(torch.zeros(param[0])))
+
+                # must set requires_grad=False
+                running_mean = nn.Parameter(torch.zeros(param[0]), requires_grad=False)
+                running_var = nn.Parameter(torch.ones(param[0]), requires_grad=False)
+                self.vars_bn.extend([running_mean, running_var])
+
+
+            elif name in ['tanh', 'relu', 'upsample', 'avg_pool2d', 'max_pool2d',
+                          'flatten', 'reshape', 'leakyrelu', 'sigmoid']:
+                continue
+            else:
+                raise NotImplementedError
+
+    def extra_repr(self):
+        info = ''
+
+        for name, param in self.config:
+            if name is 'conv2d':
+                tmp = 'conv2d:(ch_in:%d, ch_out:%d, k:%dx%d, stride:%d, padding:%d)'\
+                      %(param[1], param[0], param[2], param[3], param[4], param[5],)
+                info += tmp + '\n'
+
+            elif name is 'convt2d':
+                tmp = 'convTranspose2d:(ch_in:%d, ch_out:%d, k:%dx%d, stride:%d, padding:%d)'\
+                      %(param[0], param[1], param[2], param[3], param[4], param[5],)
+                info += tmp + '\n'
+
+            elif name is 'linear':
+                tmp = 'linear:(in:%d, out:%d)'%(param[1], param[0])
+                info += tmp + '\n'
+
+            elif name is 'leakyrelu':
+                tmp = 'leakyrelu:(slope:%f)'%(param[0])
+                info += tmp + '\n'
+
+
+            elif name is 'avg_pool2d':
+                tmp = 'avg_pool2d:(k:%d, stride:%d, padding:%d)'%(param[0], param[1], param[2])
+                info += tmp + '\n'
+            elif name is 'max_pool2d':
+                tmp = 'max_pool2d:(k:%d, stride:%d, padding:%d)'%(param[0], param[1], param[2])
+                info += tmp + '\n'
+            elif name in ['flatten', 'tanh', 'relu', 'upsample', 'reshape', 'sigmoid', 'use_logits', 'bn']:
+                tmp = name + ':' + str(tuple(param))
+                info += tmp + '\n'
+            else:
+                raise NotImplementedError
+
+        return info
+
+    def forward(self, x, vars=None, bn_training=True):
+        """
+        This function can be called by finetunning, however, in finetunning, we dont wish to update
+        running_mean/running_var. Thought weights/bias of bn is updated, it has been separated by fast_weights.
+        Indeed, to not update running_mean/running_var, we need set update_bn_statistics=False
+        but weight/bias will be updated and not dirty initial theta parameters via fast_weiths.
+        :param x: [b, 1, 28, 28]
+        :param vars:
+        :param bn_training: set False to not update
+        :return: x, loss, likelihood, kld
+        """
+
+        if vars is None:
+            vars = self.vars
+
+        idx = 0
+        bn_idx = 0
+
+        for name, param in self.config:
+            if name is 'conv2d':
+                w, b = vars[idx], vars[idx + 1]
+                # remember to keep synchrozied of forward_encoder and forward_decoder!
+                x = F.conv2d(x, w, b, stride=param[4], padding=param[5])
+                idx += 2
+                # print(name, param, '\tout:', x.shape)
+            elif name is 'convt2d':
+                w, b = vars[idx], vars[idx + 1]
+                # remember to keep synchrozied of forward_encoder and forward_decoder!
+                x = F.conv_transpose2d(x, w, b, stride=param[4], padding=param[5])
+                idx += 2
+                # print(name, param, '\tout:', x.shape)
+            elif name is 'linear':
+                w, b = vars[idx], vars[idx + 1]
+                x = F.linear(x, w, b)
+                idx += 2
+                # print('forward:', idx, x.norm().item())
+            elif name is 'bn':
+                w, b = vars[idx], vars[idx + 1]
+                running_mean, running_var = self.vars_bn[bn_idx], self.vars_bn[bn_idx+1]
+                x = F.batch_norm(x, running_mean, running_var, weight=w, bias=b, training=bn_training)
+                idx += 2
+                bn_idx += 2
+
+            elif name is 'flatten':
+                # print(x.shape)
+                x = x.view(x.size(0), -1)
+            elif name is 'reshape':
+                # [b, 8] => [b, 2, 2, 2]
+                x = x.view(x.size(0), *param)
+            elif name is 'relu':
+                x = F.relu(x, inplace=param[0])
+            elif name is 'leakyrelu':
+                x = F.leaky_relu(x, negative_slope=param[0], inplace=param[1])
+            elif name is 'tanh':
+                x = F.tanh(x)
+            elif name is 'sigmoid':
+                x = torch.sigmoid(x)
+            elif name is 'upsample':
+                x = F.upsample_nearest(x, scale_factor=param[0])
+            elif name is 'max_pool2d':
+                x = F.max_pool2d(x, param[0], param[1], param[2])
+            elif name is 'avg_pool2d':
+                x = F.avg_pool2d(x, param[0], param[1], param[2])
+
+            else:
+                raise NotImplementedError
+
+        # make sure variable is used properly
+        assert idx == len(vars)
+        assert bn_idx == len(self.vars_bn)
+
+        return x
+
+    def zero_grad(self, vars=None):
+        """
+
+        :param vars:
+        :return:
+        """
+        with torch.no_grad():
+            if vars is None:
+                for p in self.vars:
+                    if p.grad is not None:
+                        p.grad.zero_()
+            else:
+                for p in vars:
+                    if p.grad is not None:
+                        p.grad.zero_()
+
+    def parameters(self):
+        """
+        override this function since initial parameters will return with a generator.
+        :return:
+        """
+        return self.vars
+
+
+class Meta(nn.Module):
+    """
+    Meta Learner
+    """
+    def __init__(self, args, config):
+        """
+
+        :param args:
+        """
+        super(Meta, self).__init__()
+
+        self.update_lr = args.update_lr
+        self.meta_lr = args.meta_lr
+        self.n_way = args.n_way
+        self.k_spt = args.k_spt
+        self.k_qry = args.k_qry
+        self.task_num = args.task_num
+        self.update_step = args.update_step
+        self.update_step_test = args.update_step_test
+
+        self.net = Learner(config, args.imgc, args.imgsz)
+        self.meta_optim = optim.Adam(self.net.parameters(), lr=self.meta_lr)
+
+    def clip_grad_by_norm_(self, grad, max_norm):
+        """
+        in-place gradient clipping.
+        :param grad: list of gradients
+        :param max_norm: maximum norm allowable
+        :return:
+        """
+
+        total_norm = 0
+        counter = 0
+        for g in grad:
+            param_norm = g.data.norm(2)
+            total_norm += param_norm.item() ** 2
+            counter += 1
+        total_norm = total_norm ** (1. / 2)
+
+        clip_coef = max_norm / (total_norm + 1e-6)
+        if clip_coef < 1:
+            for g in grad:
+                g.data.mul_(clip_coef)
+
+        return total_norm/counter
+
+    def forward(self, x_spt, y_spt, x_qry, y_qry):
+        """
+
+        :param x_spt:   [b, setsz, c_, h, w]
+        :param y_spt:   [b, setsz]
+        :param x_qry:   [b, querysz, c_, h, w]
+        :param y_qry:   [b, querysz]
+        :return:
+        """
+        task_num, setsz, c_, h, w = x_spt.size()
+        querysz = x_qry.size(1)
+
+        losses_q = [0 for _ in range(self.update_step + 1)]  # losses_q[i] is the loss on step i
+        corrects = [0 for _ in range(self.update_step + 1)]
+
+        for i in range(task_num):
+
+            # 1. run the i-th task and compute loss for k=0
+            logits = self.net(x_spt[i], vars=None, bn_training=True)
+            loss = F.cross_entropy(logits, y_spt[i])
+            grad = torch.autograd.grad(loss, self.net.parameters())
+            fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, self.net.parameters())))
+
+            # this is the loss and accuracy before first update
+            with torch.no_grad():
+                # [setsz, nway]
+                logits_q = self.net(x_qry[i], self.net.parameters(), bn_training=True)
+                loss_q = F.cross_entropy(logits_q, y_qry[i])
+                losses_q[0] += loss_q
+
+                pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
+                correct = torch.eq(pred_q, y_qry[i]).sum().item()
+                corrects[0] = corrects[0] + correct
+
+            # this is the loss and accuracy after the first update
+            with torch.no_grad():
+                # [setsz, nway]
+                logits_q = self.net(x_qry[i], fast_weights, bn_training=True)
+                loss_q = F.cross_entropy(logits_q, y_qry[i])
+                losses_q[1] += loss_q
+                # [setsz]
+                pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
+                correct = torch.eq(pred_q, y_qry[i]).sum().item()
+                corrects[1] = corrects[1] + correct
+
+            for k in range(1, self.update_step):
+                # 1. run the i-th task and compute loss for k=1~K-1
+                logits = self.net(x_spt[i], fast_weights, bn_training=True)
+                loss = F.cross_entropy(logits, y_spt[i])
+                # 2. compute grad on theta_pi
+                grad = torch.autograd.grad(loss, fast_weights)
+                # 3. theta_pi = theta_pi - train_lr * grad
+                fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, fast_weights)))
+
+                logits_q = self.net(x_qry[i], fast_weights, bn_training=True)
+                # loss_q will be overwritten and just keep the loss_q on last update step.
+                loss_q = F.cross_entropy(logits_q, y_qry[i])
+                losses_q[k + 1] += loss_q
+
+                with torch.no_grad():
+                    pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
+                    correct = torch.eq(pred_q, y_qry[i]).sum().item()  # convert to numpy
+                    corrects[k + 1] = corrects[k + 1] + correct
+
+
+
+        # end of all tasks
+        # sum over all losses on query set across all tasks
+        loss_q = losses_q[-1] / task_num
+
+        # optimize theta parameters
+        self.meta_optim.zero_grad()
+        loss_q.backward()
+        # print('meta update')
+        # for p in self.net.parameters()[:5]:
+        # 	print(torch.norm(p).item())
+        self.meta_optim.step()
+
+
+        accs = np.array(corrects) / (querysz * task_num)
+
+        return accs
+
+    def finetunning(self, x_spt, y_spt, x_qry, y_qry):
+        """
+
+        :param x_spt:   [setsz, c_, h, w]
+        :param y_spt:   [setsz]
+        :param x_qry:   [querysz, c_, h, w]
+        :param y_qry:   [querysz]
+        :return:
+        """
+        assert len(x_spt.shape) == 4
+
+        querysz = x_qry.size(0)
+
+        corrects = [0 for _ in range(self.update_step_test + 1)]
+
+        # in order to not ruin the state of running_mean/variance and bn_weight/bias
+        # we finetunning on the copied model instead of self.net
+        net = deepcopy(self.net)
+
+        # 1. run the i-th task and compute loss for k=0
+        logits = net(x_spt)
+        loss = F.cross_entropy(logits, y_spt)
+        grad = torch.autograd.grad(loss, net.parameters())
+        fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, net.parameters())))
+
+        # this is the loss and accuracy before first update
+        with torch.no_grad():
+            # [setsz, nway]
+            logits_q = net(x_qry, net.parameters(), bn_training=True)
+            # [setsz]
+            pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
+            # scalar
+            correct = torch.eq(pred_q, y_qry).sum().item()
+            corrects[0] = corrects[0] + correct
+
+        # this is the loss and accuracy after the first update
+        with torch.no_grad():
+            # [setsz, nway]
+            logits_q = net(x_qry, fast_weights, bn_training=True)
+            # [setsz]
+            pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
+            # scalar
+            correct = torch.eq(pred_q, y_qry).sum().item()
+            corrects[1] = corrects[1] + correct
+
+        for k in range(1, self.update_step_test):
+            # 1. run the i-th task and compute loss for k=1~K-1
+            logits = net(x_spt, fast_weights, bn_training=True)
+            loss = F.cross_entropy(logits, y_spt)
+            # 2. compute grad on theta_pi
+            grad = torch.autograd.grad(loss, fast_weights)
+            # 3. theta_pi = theta_pi - train_lr * grad
+            fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, fast_weights)))
+
+            logits_q = net(x_qry, fast_weights, bn_training=True)
+            # loss_q will be overwritten and just keep the loss_q on last update step.
+            loss_q = F.cross_entropy(logits_q, y_qry)
+
+            with torch.no_grad():
+                pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
+                correct = torch.eq(pred_q, y_qry).sum().item()  # convert to numpy
+                corrects[k + 1] = corrects[k + 1] + correct
+
+
+        del net
+
+        accs = np.array(corrects) / querysz
+
+        return accs
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ shared/new_datasets.py
@@ -0,0 +1,107 @@
+import torch
+import pandas as pd
+import os
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.transforms import transforms
+from pathlib import Path
+
+
+class NovelMimicCxrJpg(Dataset):
+    """
+    Mimic-CXR-JPG Database
+    Todo: Insert references to the database here!
+    """
+
+    def __init__(self, root, path_csv, n_way, k_shot, k_query, num_episodes, resize=224):
+        self.dict_labels = {
+            'Enlarged Cardiomediastinum': 0,
+            'Fracture': 1,
+            'Lung Lesion': 2,
+            'Lung Opacity': 3,
+            'Pleural Effusion': 4,
+            'Pneumothorax': 5
+        }
+        self.root = root
+        csv_data = pd.read_csv(path_csv)  # Raw CSV data
+        data = csv_data[csv_data.split == "novel"]  # Filters for novel classes
+        self.data = data.assign(
+            labels=data["labels"].apply(lambda x: self.dict_labels[x]))  # Converts classes to numeric values
+        self.n_way = n_way
+        self.k_shot = k_shot
+        self.k_query = k_query
+        self.num_episodes = num_episodes
+        self.resize = resize
+        self.transform = transforms.Compose([lambda x: Image.open(x).convert('L'),
+                                             transforms.Resize((self.resize, self.resize)),
+                                             transforms.ToTensor()
+                                             ])
+
+        # Create Episodes
+        self.support_episodes = []  # List of training episodes (support set)
+        self.query_episodes = []  # List of testing episodes (query set)
+        for i in range(self.num_episodes):  # for each batch
+            # 1.select n_way classes randomly
+            selected_cls = np.random.choice(len(self.dict_labels), self.n_way, False)  # no duplicate
+            np.random.shuffle(selected_cls)
+            df_support = pd.DataFrame()
+            df_query = pd.DataFrame()
+            for cls in selected_cls:
+                df_cls = self.data[self.data.labels == cls]
+                # 2. select k_shot + k_query for each class
+                selected_idx = np.random.choice(len(df_cls), self.k_shot + self.k_query, False)
+                np.random.shuffle(selected_idx)
+
+                # Index of samples for the support and query set
+                support_idx = selected_idx[:self.k_shot]
+                query_idx = selected_idx[self.k_shot:]
+
+                df_support = df_support.append(df_cls.iloc[support_idx])
+                df_query = df_query.append(df_cls.iloc[query_idx])
+
+            # Shuffle the indexes so that it is no longer ordered by class
+            df_support = df_support.sample(frac=1)
+            df_query = df_query.sample(frac=1)
+
+            self.support_episodes.append(df_support)
+            self.query_episodes.append(df_query)
+
+    def __len__(self):
+        return self.num_episodes
+
+    def __getitem__(self, idx):
+        support_set = self.support_episodes[idx]
+        query_set = self.query_episodes[idx]
+
+        # Labels ranging from 0 to (number of classes -1)
+        support_labels = support_set.labels.tolist()
+        query_labels = query_set.labels.tolist()
+
+        # Convert labels to range from 0 to (n way-1) for loss calculation
+        unique_labels = np.unique(support_labels) # Unique labels are the same for support and query set
+        converted_support_labels = support_labels
+        converted_query_labels = query_labels
+        for idx, val in enumerate(unique_labels):
+            # Get indexes of labels that are equal to the iterated val
+            idx_support = [x for x, label in enumerate(support_labels) if label == val]
+            idx_query = [x for x, label in enumerate(query_labels) if label == val]
+
+            # Replace old labels with new labels
+            for idx_change in range(len(idx_support)):
+                converted_support_labels[idx_support[idx_change]] = idx
+
+            for idx_change in range(len(idx_query)):
+                converted_query_labels[idx_query[idx_change]] = idx
+
+        support_imgs = torch.Tensor()
+        for i in range(len(support_set)):
+            img_path = os.path.join(self.root, support_set.iloc[i, 0])
+            support_imgs = torch.cat((support_imgs, self.transform(img_path)[None]))  # 'None' index to add channel dim
+
+        query_imgs = torch.Tensor()
+        for j in range(len(query_set)):
+            img_path = os.path.join(self.root, query_set.iloc[j, 0])
+            query_imgs = torch.cat((query_imgs, self.transform(img_path)[None]))  # 'None' index to add channel dim
+
+        return support_imgs, torch.LongTensor(support_labels), query_imgs, torch.LongTensor(query_labels)
